9ae4c0308e6865de17f3443317afbb4b4b81b1eb
Crabstove <sony.sergey@gmail.com>
2021-01-05 23:49:13 -0500

    uploading capstone project to my repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# LinkUpFinal
+
+A repo for capstone project
+
+Link to the app
+https://linkupapp.netlify.app/
---
+++ linkup-client/src/pages/home.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import Grid  from "@material-ui/core/Grid"; 
+import PropTypes from 'prop-types'
+
+import Post from '../components/Post';
+import Profile from '../components/Profile';
+import PostSkeleton from '../util/PostSkeleton';
+
+
+import { connect } from 'react-redux';
+import { getPosts } from '../redux/actions/dataActions';
+
+class home extends Component {
+  componentDidMount(){
+    this.props.getPosts();
+
+    // Alejandro's code from before(6 days ago), looks like this fix isn't needed anymore, I will leave it here for now just in case
+    /*axios.defaults.baseURL = 'https://us-central1-linkup-ed6c5.cloudfunctions.net/api';
+    axios.get('/posts')
+    .then(res => {
+      this.setState({
+        posts: res.data
+      })
+    })
+    .catch(err => console.log(err));*/
+  }
+  render() {
+    const { posts, loading } = this.props.data;
+    let recentPostsMarkup = !loading ? (
+    posts.map((post) => <Post key={post.postId} post={post}/>)
+    ):( 
+      <PostSkeleton/>
+      );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm ={8} xs={12}>
+          {recentPostsMarkup}
+        </Grid>
+        <Grid item sm ={4} xs={12}>
+          <Profile/>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+home.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+  data: state.data
+})
+
+export default connect(mapStateToProps, { getPosts })(home);
+ 
